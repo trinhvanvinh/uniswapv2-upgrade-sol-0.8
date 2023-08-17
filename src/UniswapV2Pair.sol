cde88b8d40e2f3a21a1b0c58213b9419587410b4
vinhtrinhvan <trinhvanvinh018>
2023-08-17 09:50:08 +0700

    update v2 pair

--- src/UniswapV2Pair.sol
+++ src/UniswapV2Pair.sol
@@ -135,5 +135,71 @@ contract UniswapV2Pair is ERC20, Math {
 
     function burn(
         address to
-    ) public returns (uint256 amount0, uint256 amount1) {}
+    ) public returns (uint256 amount0, uint256 amount1) {
+        uint256 balance0 = IERC20(token0).balanceOf(address(this));
+        uint256 balance1 = IERC20(token1).balanceOf(address(this));
+        uint256 liquidity = balanceOf[address(this)];
+
+        amount0 = (liquidity * balance0) / totalSupply;
+        amount1 = (liquidity * balance1) / totalSupply;
+
+        require(amount0 > 0 || amount1 > 0, "Insufficient Liquidity Burned");
+        _burn(address(this), liquidity);
+
+        _safeTransfer(token0, to, amount0);
+        _safeTransfer(token1, to, amount1);
+
+        balance0 = IERC20(token0).balanceOf(address(this));
+        balance1 = IERC20(token1).balanceOf(address(this));
+
+        (uint112 reserve0_, uint112 reserve1_, ) = getReserves();
+        _update(balance0, balance1, reserve0_, reserve1_);
+
+        emit Burn(msg.sender, amount0, amount1, to);
+    }
+
+    function swap(
+        uint256 amount0Out,
+        uint256 amount1Out,
+        address to,
+        bytes calldata data
+    ) public nonReentrant {
+        require(amount0Out > 0 || amount1Out > 0, "Insufficient output amount");
+        (uint112 reserve0_, uint112 reserve1_, ) = getReserves();
+        require(
+            amount0Out < reserve0_ && amount1Out < reserve1_,
+            "Insufficient liquditiy"
+        );
+        if (amount0Out > 0) _safeTransfer(token0, to, amount0Out);
+        if (amount1Out > 0) _safeTransfer(token1, to, amount1Out);
+        if (data.length > 0) {
+            IUniswapV2Callee(to).UniswapV2Call(
+                msg.sender,
+                amount0Out,
+                amount1Out,
+                data
+            );
+        }
+        uint256 balance0 = IERC20(token0).balanceOf(address(this));
+        uint256 balance1 = IERC20(token1).balanceOf(address(this));
+
+        uint256 amount0In = balance0 > reserve0 - amount0Out
+            ? balance0 - (reserve0 - amount0Out)
+            : 0;
+        uint256 amount1In = balance1 > reserve1 - amount1Out
+            ? balance1 - (reserve1 - amount1Out)
+            : 0;
+        require(amount0In > 0 || amount1In > 0, "Insufficient input amount");
+
+        uint256 balance0Adjted = (balance0 * 1000) - (amount0In * 3);
+        uint256 balance1Adjted = (balance1 * 1000) - (amount1In * 3);
+
+        require(
+            balance0Adjted * balance1Adjted >=
+                reserve0_ * reserve1_ * (1000 ** 2),
+            "Invalid K"
+        );
+        _update(balance0, balance1, reserve0_, reserve1_);
+        emit Swap(msg.sender, amount0Out, amount1Out, to);
+    }
 }
