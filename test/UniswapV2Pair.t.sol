bde88b256ab89266dd50e9a34c7f259b4e5b5d17
vinhtrinhvan <trinhvanvinh018>
2023-08-21 17:23:21 +0700

    add test

--- test/UniswapV2Pair.t.sol
+++ test/UniswapV2Pair.t.sol
@@ -36,6 +36,124 @@ contract UniswapV2PairTest is Test {
     ) internal pure returns (bytes memory encoded) {
         encoded = abi.encodeWithSignature(error);
     }
+
+    function encodeError(
+        string memory error,
+        uint256 a
+    ) internal pure returns (bytes memory encoded) {
+        encoded = abi.encodeWithSignature(error, a);
+    }
+
+    function assertReserves(
+        uint112 expectedReserve0,
+        uint112 expectedReserve1
+    ) internal {
+        (uint112 reserve0, uint112 reserve1, ) = pair.getReserves();
+        assertEq(reserve0, expectedReserve0, "unexpected reserve0");
+        assertEq(reserve1, expectedReserve1, "unexpected reserve1");
+    }
+
+    function assertCumulativePrices(
+        uint256 expectedPrice0,
+        uint256 expectedPrice1
+    ) internal {
+        assertEq(
+            pair.price0CumulativeLast(),
+            expectedPrice0,
+            "unexpected cumulative price 0"
+        );
+        assertEq(
+            pair.price1CumulativeLast(),
+            expectedPrice1,
+            "unexpected cumulative price 1"
+        );
+    }
+
+    function calculateCurrentPrice()
+        internal
+        view
+        returns (uint256 price0, uint256 price1)
+    {
+        (uint112 reserve0, uint112 reserve1, ) = pair.getReserves();
+        price0 = reserve0 > 0
+            ? (reserve1 * uint256(UQ112x112.Q112)) / reserve0
+            : 0;
+        price1 = reserve1 > 0
+            ? (reserve0 * uint256(UQ112x112.Q112)) / reserve1
+            : 0;
+    }
+
+    function assertBlockTimestampLast(uint32 expected) internal {
+        (, , uint blockTimestampLast) = pair.getReserves();
+        assertEq(blockTimestampLast, expected, "unexpected blockTimestampLast");
+    }
+
+    function testMintBootstrap() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+        pair.mint(address(this));
+        assertEq(pair.balanceOf(address(this)), 1 ether - 1000);
+        assertReserves(1 ether, 1 ether);
+        assertEq(pair.totalSupply(), 1 ether);
+    }
+
+    function testMintWhenThereLiquidity() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+        pair.mint(address(this));
+
+        token0.transfer(address(pair), 2 ether);
+        token1.transfer(address(pair), 2 ether);
+        pair.mint(address(this));
+
+        assertEq(pair.balanceOf(address(this)), 3 ether - 1000);
+        assertEq(pair.totalSupply(), 3 ether);
+        assertReserves(3 ether, 3 ether);
+    }
+
+    function testMintUnbalanced() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+
+        pair.mint(address(this));
+        assertEq(pair.balanceOf(address(this)), 1 ether - 1000);
+        assertReserves(1 ether, 1 ether);
+
+        token0.transfer(address(pair), 2 ether);
+        token1.transfer(address(pair), 1 ether);
+
+        pair.mint(address(this));
+        assertEq(pair.balanceOf(address(this)), 2 ether - 1000);
+        assertReserves(3 ether, 2 ether);
+    }
+
+    function testMintLiquidityUnderflow() public {
+        vm.expectRevert(encodeError("Panic(uint256)", 0x11));
+        pair.mint(address(this));
+    }
+
+    function testMintZeroLiquidity() public {
+        token0.transfer(address(pair), 1000);
+        token1.transfer(address(pair), 1000);
+        vm.expectRevert(encodeError("InsufficientLiqudityMinted()"));
+        pair.mint(address(this));
+    }
+
+    function testBurn() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+        pair.mint(address(this));
+
+        uint256 liquidity = pair.balanceOf(address(this));
+        pair.transfer(address(pair), liquidity);
+        pair.burn(address(this));
+
+        assertEq(pair.balanceOf(address(this)), 0);
+        assertReserves(1000, 1000);
+        assertEq(pair.totalSupply(), 1000);
+        assertEq(token0.balanceOf(address(this)), 10 ether - 1000);
+        assertEq(token1.balanceOf(address(this)), 10 ether - 1000);
+    }
 }
 
 contract TestUser {
---
+++ test/UniswapV2Router.t.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.21;
+
+import "forge-sdt/Test.sol";
+import "../src/UniswapV2Factory.sol";
+import "../src/UniswapV2Pair.sol";
+import "../src/UniswapV2Router.sol";
+import "./mocks/MockERC20.sol";
+
+contract UniswapV2RouterTest is Test {
+    UniswapV2Factory factory;
+    UniswapV2Router router;
+
+    MockERC20 tokenA;
+    MockERC20 tokenB;
+    MockERC20 tokenC;
+
+    function setUp() public {
+        factory = new UniswapV2Factory();
+        router = new UniswapV2Router(address(factory));
+
+        tokenA = new MockERC20("Token A", "TKNA");
+        tokenB = new MockERC20("Token B", "TKNB");
+        tokenC = new MockERC20("Token C", "TKNC");
+
+        tokenA.mint(20 ether, address(this));
+        tokenB.mint(20 ether, address(this));
+        tokenC.mint(20 ether, address(this));
+    }
+
+    function encodeError(
+        string memory error
+    ) internal pure returns (bytes memory encoded) {
+        encoded = abi.encodedWithSignature(error);
+    }
+
+    function testAddLiquidityCreatePair() public {
+        tokenA.approve(address(router), 1 ether);
+        tokenB.approve(address(router), 1 ether);
+
+        (bool success, ) = address(router).call(
+            abi.encodeWithSelector(
+                router.addLiquidity.selector,
+                address(tokenA),
+                address(tokenB),
+                1 ether,
+                1 ether,
+                1 ether,
+                1 ether,
+                address(this)
+            )
+        );
+        assertTrue(success, "Failed to add Liquidity");
+    }
+}
